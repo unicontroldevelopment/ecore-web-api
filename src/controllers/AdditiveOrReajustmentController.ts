94b14082c76a399db461a8120ca37bcbe0796801
guitxdasilva <guilhermetxdasilva@gmail.com>
2024-10-01 16:37:02 -0300

    ECW2 - Additives Fix

--- src/controllers/AdditiveOrReajustmentController.ts
+++ src/controllers/AdditiveOrReajustmentController.ts
@@ -66,7 +66,7 @@ class AdditiveOrReajustmentController {
         return res.status(500).json({ message: "Aditivo não encontrado!" });
       }
 
-      const {additive_Clauses, id, contract_id, ...updateData} = await req.body;
+      const {additive_Clauses, id, contract_id, d4SignData, ...updateData} = await req.body;
 
       if(updateData.newValue && updateData.oldValue) {
         updateData.newValue = parseFloat(updateData.newValue.replace(/\./g, '').replace(',', '.')).toString();
